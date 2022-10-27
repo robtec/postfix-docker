157014ceeab48979fe59fc2353e02719e57cf190
Rob <rob.p.tec@gmail.com>
2022-10-27 21:55:22 +0100

    added users to server

--- Dockerfile
+++ Dockerfile
@@ -4,13 +4,15 @@ ENV DEBIAN_FRONTEND=noninteractive
 
 RUN apt-get update -y && \
     apt-get install --no-install-recommends -y \
-    ca-certificates postfix mailutils && \
+    ca-certificates postfix mailutils diceware && \
     apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 RUN adduser mailbucket --quiet --disabled-password \
     --shell /usr/sbin/nologin --gecos "Mail Bucket"
 
 COPY entrypoint.sh .
+
+COPY users.txt .
 
 COPY main.cf /etc/postfix/main.cf
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,24 @@
 
 set -e
 
+USERS_FILE="users.txt"
+
+function load_users() {
+
+    while read username || [ -n "$username" ]; do
+        echo -e "Setting up system user ${username}"
+        adduser "$username" --quiet --disabled-password --shell /usr/sbin/nologin --gecos "" --force-badname || true
+        password=$(diceware -d-)
+        echo "$username:$password" | chpasswd || true
+    done < $USERS_FILE
+    
+}
+
+# check if users file exists
+if [ -f "$USERS_FILE" ]; then
+    echo -e "Found users file to process"
+    load_users
+fi
+
+
 exec "$@"
